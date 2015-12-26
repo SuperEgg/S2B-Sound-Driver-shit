280bc3bc51145d2e1d3f0b4e8b10a9b6d235444e
Clownacy <Clownacy@users.noreply.github.com>
2015-12-26 22:34:10 +0000

    Fixing Z80 code listing
    Previously, the listing file was empty, because the second call to asl.exe overwrote it

--- Assemble.bat
+++ Assemble.bat
@@ -1,10 +1,8 @@
 @Echo Off
 
-EXE\asl -q -cpu Z80 -gnuerrors -c -A -xx Z80.asm
+EXE\asl -cpu Z80 -gnuerrors -c -A -L -xx Z80.asm
 EXE\p2bin Z80.p Z80.bin -r 0x-0x
 IF NOT EXIST Z80.p goto DPERROR
-CLS
-EXE\asl -L z80.lst
 KENS\KensSharp.exe -n -c Saxman Z80.bin Z80.sax
  
 :DPERROR
